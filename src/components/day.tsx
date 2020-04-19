a55dd44ffbd9f830dbaafcdeb2049801ed54dcb5
Iris <learncodecool@gmail.com>
2020-04-19 08:46:20 +0800

    資料及日期無資料灰色顯示

--- src/components/day.tsx
+++ src/components/day.tsx
@@ -13,13 +13,18 @@ function DayCreater(props: I_Props) {
     const { _date, initTimeTable, bookedIndex, availableIndex, UserLang } = props
     const today = dayjs()
     const cn_Week = ["日", "一", "二", "三", "四", "五", "六"]
+
     return (
         <div className="day_container">
-            <div className={cx("day_header", { expired: _date.isBefore(today) })}>
+            <div
+                className={cx("day_header", {
+                    expired: _date.isBefore(today.add(-1, "day")) || (!bookedIndex && !availableIndex),
+                })}
+            >
                 <div className="weekday">{UserLang === "zh-TW" ? cn_Week[_date.day()] : _date.format("ddd")}</div>
                 <div className="date">{_date.format("DD")}</div>
             </div>
-            <div className={cx("day_body", { expired: _date.isBefore(today) })}>
+            <div className={cx("day_body", { expired: _date.isBefore(today.add(-1, "day")) })}>
                 {initTimeTable.map((time, i) => {
                     return (
                         <div
